96b96b589746b8b22a1b635d69e9155ed44d7463
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-07 14:01:57 +0200

    consolelog

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -1,6 +1,5 @@
 import {Core} from '../../node_modules/phaser/src/phaser-arcade-physics'
 
-var game = new Core.Game(800, 400, Core.Auto)
 
 function onSignIn(googleUser : any) {
     var profile = googleUser.getBasicProfile();
