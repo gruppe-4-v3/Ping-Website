549d459c5d43ff386141f7bcb92274c4db868477
Sofus Nielsen <sofushn@hotmail.dk>
2019-05-07 22:03:27 +0200

    Added basic game canvas

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -1,3 +1,15 @@
+import * as Phaser from "phaser";
+
+let config: GameConfig = {
+  title: "Starfall",
+  width: 800,
+  height: 600,
+  parent: "game",
+  backgroundColor: "#18216D"
+}
+
+let game: Phaser.Game = new Phaser.Game(config);
+
 function onSignIn(googleUser : any) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
