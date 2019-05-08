4f34b7788b9dd55ed6378a7a6f13e3685b59c17d
sofu0185 <sofu0185@edu.easj.dk>
2019-05-08 09:54:50 +0200

    Created GameScene class

---
+++ PingWebsite/src/js/GameScene.ts
@@ -0,0 +1,20 @@
+export class GameScene extends Phaser.Scene {
+
+    constructor() {
+        super({
+            key: "GameScene"
+        });
+
+    }
+    preload (): void
+    {
+        }
+
+    create (): void
+    {
+    }
+
+    update (): void
+    {
+    }
+}
