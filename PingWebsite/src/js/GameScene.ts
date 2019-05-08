9846ff09ebb181087e5f024e73a3353b0e9753b9
sofus0185 <sofu0185@edu.easj.dk>
2019-05-08 23:15:29 +0200

    Simplified spawnBall method

--- PingWebsite/src/js/GameScene.ts
+++ PingWebsite/src/js/GameScene.ts
@@ -32,36 +32,40 @@ export class GameScene extends Phaser.Scene {
 
         let object: GameObjects.GameObject = this.physics.add.existing(this.player)
         if(this.player.body instanceof Phaser.Physics.Arcade.Body){
-        if(this.cursor.left.isDown)// move left if the left key is pressed
-        {
+            if(this.cursor.left.isDown)// move left if the left key is pressed
+            {
             this.player.body.velocity.x = -100;
+            }
+            else if(this.cursor.right.isDown)// move right if the right key is pressed
+            {
+                this.player.body.velocity.x = 100;
+            }
+            else//stop if no key is pressed.
+            {
+                this.player.body.velocity.x = 0;
+            }
+            this.player.body.collideWorldBounds = true;
         }
-        else if(this.cursor.right.isDown)// move right if the right key is pressed
-        {
-            this.player.body.velocity.x = 100;
-        }
-        else//stop if no key is pressed.
-        {
-            this.player.body.velocity.x = 0;
-        }
-        this.player.body.collideWorldBounds = true;
-    }
     }
 
+    // Spawns a ball object in the gamescene
     private spawnBall(): void {
         let spawnPoint = { x: Phaser.Math.Between(25, 775), y: 50 }
         let size: number = 15
-        let colour: number = 0xff0000
-        let ball: Phaser.GameObjects.Arc = this.add.circle(spawnPoint.x, spawnPoint.y, size, colour)
-        
-        let object: GameObjects.GameObject = this.physics.add.existing(ball)
-        if(ball.body instanceof Phaser.Physics.Arcade.Body){
-            ball.body.velocity.x = 100
-            ball.body.velocity.y = 100
-            ball.body.bounce.x = 1
-            ball.body.bounce.y = 1
-            ball.body.collideWorldBounds = true
-        }
+        let color: number = 0xff0000
+
+        // add ball to the GameScene rendere
+        let ball: Phaser.GameObjects.Arc = this.add.circle(spawnPoint.x, spawnPoint.y, size, color)
+
+        // give ball an arcade physics body
+        this.physics.add.existing(ball)
+
+        let ballBody: Phaser.Physics.Arcade.Body = <Phaser.Physics.Arcade.Body>ball.body
+        ballBody.velocity.x = 100
+        ballBody.velocity.y = 100
+        ballBody.bounce.x = 1
+        ballBody.bounce.y = 1
+        ballBody.collideWorldBounds = true
     }
 
     private spawnPlayer(): void
