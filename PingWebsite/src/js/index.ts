b66c974e6b2779f089724517cf2965508ddba92f
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-08 13:27:02 +0200

    window

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -18,7 +18,16 @@ let game: Phaser.Game = new Phaser.Game(config);
 let SignIn: HTMLDivElement = <HTMLDivElement> document.getElementById("signin");
 //SignIn.addEventListener('data-onsuccess', onSignIn)
 //gapi.auth2.getAuthInstance().currentUser.get().isSignedIn()
-function onSignInts() {  
+(<any>window).onSignInts = () => {
+  var auth2 = gapi.auth2.getAuthInstance().currentUser.get();
+  var profile = auth2.getBasicProfile();
+  console.log('TS')
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+};
+function onSignInts1() {  
   var auth2 = gapi.auth2.getAuthInstance().currentUser.get();
   var profile = auth2.getBasicProfile();
   console.log('TS')
