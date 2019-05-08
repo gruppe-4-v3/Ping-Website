0ee5f39c153bc778fa6990ce34d870f3efcebf8a
Mads Meinert Andersen <mads485d@edu.easj.dk>
2019-05-08 16:17:47 +0200

    Har tilføjet player og slettet Lifelogic(sorry Daniel, det fuckede min computer :-1: )

--- PingWebsite/src/js/GameScene.ts
+++ PingWebsite/src/js/GameScene.ts
@@ -8,6 +8,9 @@ export class GameScene extends Phaser.Scene {
         });
     }
 
+    cursor: Phaser.Input.Keyboard.CursorKeys
+    player: Phaser.GameObjects.Rectangle
+
     // Loads all assets from files into memory
     preload (): void
     {
@@ -17,12 +20,32 @@ export class GameScene extends Phaser.Scene {
     create (): void
     {
         this.spawnBall()
+        this.cursor = this.input.keyboard.createCursorKeys();
+        this.spawnPlayer();
+
+
     }
 
     // Updates every game tick, cointains dynamic 
     update (): void
     {
 
+        let object: GameObjects.GameObject = this.physics.add.existing(this.player)
+        if(this.player.body instanceof Phaser.Physics.Arcade.Body){
+        if(this.cursor.left.isDown)// move left if the left key is pressed
+        {
+            this.player.body.velocity.x = -100;
+        }
+        else if(this.cursor.right.isDown)// move right if the right key is pressed
+        {
+            this.player.body.velocity.x = 100;
+        }
+        else//stop if no key is pressed.
+        {
+            this.player.body.velocity.x = 0;
+        }
+        this.player.body.collideWorldBounds = true;
+    }
     }
 
     private spawnBall(): void {
@@ -39,5 +62,12 @@ export class GameScene extends Phaser.Scene {
             ball.body.bounce.y = 1
             ball.body.collideWorldBounds = true
         }
+    }
+
+    private spawnPlayer(): void
+    {
+        this.player = this.add.rectangle(400, 580, 100, 10, 0xff000)
+
+
     }
 }
