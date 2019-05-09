c9ac18a9a2d57c4b1e200b267f375fd05c031806
Emil Hammer <mr_h4mm3r@live.dk>
2019-05-09 13:51:35 +0200

    IUser to IUsers and IUsers' options to optional

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -7,10 +7,7 @@ import { IUsers } from "../js/IUsers"
 
 var userID : string = "";
 var userName : string = "";
-interface IUser {
-  Id : string;
-  Username: string;
-}
+
 let config: GameConfig = {
   title: "Ping (Name Subject to Change)",
   width: 800,
@@ -40,11 +37,11 @@ function signinfunc() {
   getUser(userID);
 }
 function getUser(id : string){
-  axios.get<IUser>('https://pingwebapi.azurewebsites.net/api/users/' + id)
+  axios.get<IUsers>('https://pingwebapi.azurewebsites.net/api/users/' + id)
 .then(function(response){
-  console.log(response.data.Username); // ex.: { user: 'Your User'}
+  console.log(response.data.username); // ex.: { user: 'Your User'}
   console.log(response.status); // ex.: 200
-  if (response.data.Username != userName) {
+  if (response.data.username != userName) {
     postUser()
   }
 })
