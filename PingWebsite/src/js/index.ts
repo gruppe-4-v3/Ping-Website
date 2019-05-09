31d9c708c7aa2d28f9ef1ab108bead92c4e8226f
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-09 12:30:57 +0200

    dist

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -1,6 +1,9 @@
 import * as Phaser from "phaser";
 import { GameScene } from "./GameScene"
 
+var userID : number = 0;
+var userName : string = "";
+
 let config: GameConfig = {
   title: "Ping (Name Subject to Change)",
   width: 800,
@@ -17,12 +20,15 @@ document.getElementById('startGameBtn').addEventListener('click', function() {
   let game: Phaser.Game = new Phaser.Game(config);
   document.getElementById('startGameBtn').remove()
 })
-var but : HTMLDivElement = <HTMLDivElement> document.getElementById("signin2");
-but.addEventListener('sign', signinfunc)
+
+var signinbut : HTMLDivElement = <HTMLDivElement> document.getElementById("signin2");
+signinbut.addEventListener('sign', signinfunc)
 
 function signinfunc() {
   console.log("in ts");
-  console.log(but.getAttribute("data-id"));
-  console.log(but.getAttribute("data-name"));
+  console.log(signinbut.getAttribute("data-id"));
+  console.log(signinbut.getAttribute("data-name"));
+  userID = +signinbut.getAttribute("data-id");
+  userName = signinbut.getAttribute("data-name");
 }
 //setInterval(signinfunc, 50);
