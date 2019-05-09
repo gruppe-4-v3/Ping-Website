60c0ac02991958f0e0af022836d184779522f27f
Emil Hammer <mr_h4mm3r@live.dk>
2019-05-09 12:54:10 +0200

    + Global Highscore

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -1,5 +1,9 @@
 import * as Phaser from "phaser";
 import { GameScene } from "./GameScene"
+import axios, { AxiosResponse } from "../../node_modules/axios/index"
+import { IScore } from "../js/IScore"
+import { IUsers } from "../js/IUsers"
+
 
 let config: GameConfig = {
   title: "Ping (Name Subject to Change)",
@@ -50,4 +54,57 @@ declare const gapi: any;
 
 function ngAfterViewInit(){
       this.googleInit();
+}
+
+window.onload = function(){
+let BtnGlobalHighscore = document.getElementById("BtnGlobalHighscore");
+BtnGlobalHighscore.addEventListener("click", getGlobalHighscore);
+}
+function getGlobalHighscore(){
+    axios.get<IScore[]>('https://pingwebapi.azurewebsites.net/api/highscore')
+  .then(function(response: AxiosResponse<IScore[]>) : void
+  {
+    createHighscoreBoard(response);
+  });  
+}
+
+/** Creates a highscoreboard
+* @param response - An AxiosResponse which is made up of an array of IScore. Should already be filtered by the REST Web API 
+*/
+function createHighscoreBoard(response: AxiosResponse<IScore[]>){
+  let ContentGlobalHighscore = document.getElementById("ContentGlobalHighscore");
+  DeleteChildnodes(ContentGlobalHighscore);
+  
+  var tableElement = document.createElement("table");
+  var tableHeaderRow = document.createElement("tr");
+  var tableHeaderUserElement = document.createElement("th");
+  var tableHeaderScoreElement = document.createElement("th");
+  
+  tableHeaderUserElement.innerHTML = "User";
+  tableHeaderScoreElement.innerHTML = "Score";
+  
+  tableHeaderRow.appendChild(tableHeaderUserElement);
+  tableHeaderRow.appendChild(tableHeaderScoreElement);
+  tableElement.appendChild(tableHeaderRow);
+
+  response.data.forEach((userscore: IScore) => {
+    var newRow = document.createElement("tr");
+    var newUser = document.createElement("td");
+    var newScore = document.createElement("td");
+    
+    newUser.innerHTML = "" + userscore.userId;
+    newScore.innerHTML = "" + userscore.score;
+    
+    newRow.appendChild(newUser);
+    newRow.appendChild(newScore);
+    tableElement.appendChild(newRow)
+  });
+  ContentGlobalHighscore.appendChild(tableElement);
+}
+
+/** Deletes all childnodes of the given element. Is primarily used to clear the highscore content before remaking a new one. */
+function DeleteChildnodes(element : Element){
+  while(element.firstChild){
+    element.removeChild(element.firstChild);
+  }
 }
