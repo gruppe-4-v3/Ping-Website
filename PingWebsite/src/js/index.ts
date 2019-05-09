1440dbcd953d3fecdad79bff43bfd825a9da06e8
Henrik Andersen <henr563a@edu.easj.dk>
2019-05-09 10:08:50 +0200

    Revert "Merge conflict"
    This reverts commit 98e7bb0c2c4993710f07b6f88dfed66a376906db.

--- PingWebsite/src/js/index.ts
+++ PingWebsite/src/js/index.ts
@@ -16,6 +16,42 @@ let config: GameConfig = {
 
 let game: Phaser.Game = new Phaser.Game(config);
 
+<<<<<<< HEAD
+let SignIn: HTMLDivElement = <HTMLDivElement> document.getElementById("signin");
+//SignIn.addEventListener('data-onsuccess', onSignIn)
+//gapi.auth2.getAuthInstance().currentUser.get().isSignedIn()
+(<any>window).onSignInts = () => {
+  var auth2 = gapi.auth2.getAuthInstance().currentUser.get();
+  var profile = auth2.getBasicProfile();
+  console.log('TS')
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+};
+function onSignInts1() {  
+  var auth2 = gapi.auth2.getAuthInstance().currentUser.get();
+  var profile = auth2.getBasicProfile();
+  console.log('TS')
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+window.onload=function(){
+  let btn = document.getElementById("globalHighscore");
+  btn.addEventListener("click", getGlobalHighscore);
+}
+
+function getGlobalHighscore(){
+  
+  axios.get('https://pingwebapi.azurewebsites.net/api/highscore')
+.then(function(response){
+  console.log(response.data); // ex.: { user: 'Your User'}
+  console.log(response.status); // ex.: 200
+});  
+}
+=======
 var auth2 : any; // The Sign-In object.
 var googleUser : any; // The current user.
 
@@ -114,3 +150,4 @@ var refreshValues = function() {
     updateGoogleUser();
   }
 }
+>>>>>>> f98f97ff8b3a52bf13c413ee1bd588910b79a7e9
