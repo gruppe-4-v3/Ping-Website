fe521de16e92852884682cf2944e73e8ac43f5a5
Emil Hammer <mr_h4mm3r@live.dk>
2019-05-16 15:45:03 +0200

    Highscore now working.

--- src/js/RESTCalls.ts
+++ src/js/RESTCalls.ts
@@ -1,27 +1,28 @@
-import axios, { AxiosResponse, AxiosError } from "../../node_modules/axios/index"
+import axios, { AxiosResponse, AxiosError, AxiosPromise } from "../../node_modules/axios/index"
 import { IScore } from "./Model/IScore"
 import { IUsers } from "./Model/IUsers"
 
 export class RESTCalls {
 
-  static getUser(id: string, name: string) {
-    axios.get<IUsers>('https://pingwebapi.azurewebsites.net/api/users/' + id)
-      .then((response) => {
-        console.log("Bruger allerede kendt: " + response.data.username + ", ID: " + response.data.id)
+  static getUser(id: string, name: string): AxiosPromise<IUsers> {
+    return axios.get<IUsers>('https://pingwebapi.azurewebsites.net/api/users/' + id)
+      .then((response: AxiosResponse<IUsers>) => {
+        return response;
       })
       .catch((error: AxiosError) => {
         console.log("Bruger ikke fundet.")
-        RESTCalls.postUser(id, name); // Creates a new user since it couldn't find one with the corresponding id.
+        return RESTCalls.postUser(id, name); // Creates a new user since it couldn't find one with the corresponding id.
       })
   }
   static postUser(userID: string, userName: string) {
-    axios.post<IUsers>('https://pingwebapi.azurewebsites.net/api/users', { Id: userID, Username: userName })
+    return axios.post<IUsers>('https://pingwebapi.azurewebsites.net/api/users', { Id: userID, Username: userName })
       .then((response: AxiosResponse) => {
         console.log("Ny bruger oprettet");
+        return response.data;
       })
       .catch((error: AxiosError) => {
-          console.log(error);
-        })
+        console.log(error);
+      })
   }
 
   static postHighscore(userID: string, score: number) {
@@ -36,10 +37,25 @@ export class RESTCalls {
   }
 
   static getGlobalHighscore() {
-    axios.get<IScore[]>('https://pingwebapi.azurewebsites.net/api/highscore')
-      .then((response: AxiosResponse<IScore[]>) => {});}
+    return axios.get<IScore[]>('https://pingwebapi.azurewebsites.net/api/highscore/top/15')
+      .then((response: AxiosResponse<IScore[]>) => {
+          return response.data.sort((n1, n2) => n2.score - n1.score)
+      })
+  }
 
   static getLocalHighscore(userID: string) {
     axios.get<IScore[]>('https://pingwebapi.azurewebsites.net/api/highscore/' + userID)
-      .then((response: AxiosResponse<IScore[]>) => {});}
+      .then((response: AxiosResponse<IScore[]>) => { });
+  }
+
+  static getUsernameFromId(id: string) {
+    return RESTCalls.getUser(id, "unused").then(response => {
+      return response.data.username;
+    })
+  }
+}
+
+interface ScoreText {
+  text: string;
+  score: number;
 }
--- src/js/Scenes/HighScoreScene.ts
+++ src/js/Scenes/HighScoreScene.ts
@@ -1,25 +1,43 @@
 import { TextButtons } from "../GameObjects/TextButtons"
+import { RESTCalls } from "../RESTCalls";
+import { IUsers } from "../Model/IUsers";
 
 export class HighScoreScene extends Phaser.Scene {
-    constructor(){
+    constructor() {
         super({
             key: 'HighScoreScene'
         })
     }
 
-    preload() : void{}
+    preload(): void { }
 
-    create() : void
-    {
-        var highScoreButton = new TextButtons(this,350,450,'Back to the Main Menu',{fill:'#f2f2f2'});
+    create(): void {
+        var highScoreButton = new TextButtons(this, 350, 450, 'Back to the Main Menu', { fill: '#f2f2f2' });
         this.add.existing(highScoreButton);
+        this.createGlobalHighscore();
         highScoreButton.on('pointerup', () => {
+
             this.scene.launch('MMenuScene');
             this.scene.stop();
         })
     }
 
-    update() : void
-    {
+    update(): void {}
+
+
+    createGlobalHighscore() {
+        RESTCalls.getGlobalHighscore().then(response => {
+            console.log("Nede i then i createGlobalHighscore");
+            let x = 0;
+            let y = 0;
+            let rang = 1;
+
+            response.forEach(element => {
+                this.add.text(x,y,"#" + rang + " - " + element.userId + ": " + element.score);
+                y = y + 20;
+                rang++;
+            })
+            
+        });
     }
 }
