a752e3e0f9b5d36369eec49039ed1493d1adf84d
DanMoll <dani948d@edu.easj.dk>
2019-05-20 14:37:50 +0100

    Variabel gameover-tekst.

--- src/js/Scenes/GameOverScene.ts
+++ src/js/Scenes/GameOverScene.ts
@@ -16,7 +16,20 @@ export class GameOverScene extends Phaser.Scene {
         finalScoreText = this.add.text(350, 340,'',{ fontSize: '32px', fill: '#000' });
         gameOverText = this.add.text(10, 200, '', { fontSize: '32px', fill: '#000' });
         finalScoreText.setText("Final Score: " + (<any>this.sys.settings.data).finalScore);
-        gameOverText.setText("You're not very good at this, are you?");
+        switch ((<any>this.sys.settings.data).finalScore) {
+            case ((<any>this.sys.settings.data).finalScore < 500):
+                gameOverText.setText("You're not very good at this, are you?");
+                break;
+            case ((<any>this.sys.settings.data).finalScore < 1000):
+                gameOverText.setText("You're getting there!");    
+                break;
+            case ((<any>this.sys.settings.data).finalScore > 1000):
+                gameOverText.setText("You could also take a walk y'know?");
+                break;
+            default:
+                gameOverText.setText("How'd you manage that?!");
+                break;
+        }
 
         var playAgainButton = new TextButtons(this,350,375,'Play again?',{fill:'#f2f2f2'});
         this.add.existing(playAgainButton);
