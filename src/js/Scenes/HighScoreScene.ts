e6d598ed884dd0e98b5c59ea479c76b2d7ee8e5e
Emil Hammer <emil629b@edu.easj.dk>
2019-05-21 15:49:25 +0200

    Highscore now showing correct time.

--- src/js/Scenes/HighScoreScene.ts
+++ src/js/Scenes/HighScoreScene.ts
@@ -31,16 +31,20 @@ export class HighScoreScene extends Phaser.Scene {
             let rang = 1;
 
             this.add.text(0,0, "Rang");
-            this.add.text(0, 50, "Navn");
-            this.add.text(0, 100, "Score");
-            this.add.text(0, 150, "Dato");
+            this.add.text(50,0, "Navn");
+            this.add.text(300,0, "Score");
+            this.add.text(400,0, "Dato");
 
             response.forEach(element => {
-                //let dateFormat = element.time.getDay + "/" + element.time.getMonth + "-" + element.time.getFullYear + " - " + element.time.getHours + ":" + element.time.getMinutes;
+                // Takes the date element and makes it to a string. Splits it up in 2 - separated at T.
+                let dateString = element.time.toString();
+                let dateStringSplit = dateString.split("T", 2)
+                let dateFormat = dateStringSplit[0] + " - " + dateStringSplit[1];
+
                 this.add.text(0, y, "#" + rang);
-                this.add.text(100, y, element.userId + ":");
-                this.add.text(200, y, "" + element.score + " :");
-                this.add.text(300, y, ""+element.time.getDate)
+                this.add.text(50, y, element.userId);
+                this.add.text(300, y, "" + element.score);
+                this.add.text(400, y, ""+ dateFormat)
 
                 y = y + 20;
                 rang++;
