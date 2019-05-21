b56840bab21c2b2361075149f02ecda043fd19be
Mads Meinert Andersen <mads485d@edu.easj.dk>
2019-05-21 13:10:14 +0200

    Random powerups og instant ball cm

--- src/js/Scenes/ChallengeMode.ts
+++ src/js/Scenes/ChallengeMode.ts
@@ -8,9 +8,15 @@ export class ChallengeMode extends GameScene {
         })
     }
 
+    
+    /** The speed of the player */
     playerSpeed: number = 1000;
 
+    /** How often a new ball spawns in seconds */ 
     ballSpawnTime = 10;
+    /** Time since last ball spawned */ 
+    lastBallTime = this.ballSpawnTime;
+    
     protected onPlayerCollide(): void
     {
         this.score++;
