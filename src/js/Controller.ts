973db4ef7998df53f27894833864993d0acfa189
sofu0185 <sofu0185@edu.easj.dk>
2019-05-22 10:45:57 +0200

    Can now recive data from pi controller

--- src/js/Controller.ts
+++ src/js/Controller.ts
@@ -11,20 +11,25 @@ export class Controller {
     ip: string;
     port: number;
     isConnected: boolean = false;
+    OnMessage: (event: MessageEvent) => any;
 
     Connect(): void{
         this.ip = (<HTMLInputElement>document.getElementById('piIp')).value;
         this.webSocketURL = 'ws://' + this.ip + ':' + this.port;
         this.webSocketClient = new WebSocket(this.webSocketURL);
         console.log('Connecting...');
-        this.webSocketClient.addEventListener('open', () => { 
-            this.isConnected = true;
-            console.log('Connection established to ' + this.ip);
-            let connectBtn = <HTMLButtonElement>document.getElementById('connectPiBtn');
-            connectBtn.innerText = 'Connected...';
-            connectBtn.setAttribute('id', 'disconnectPiBtn');
-            connectBtn.onclick = () => this.Disconnect()
-        })
+        this.webSocketClient.addEventListener('open', () => this.OnConnect())
+    }
+
+    private OnConnect() {
+        this.isConnected = true;
+        console.log('Connection established to ' + this.ip);
+        let connectBtn = <HTMLButtonElement>document.getElementById('connectPiBtn');
+        connectBtn.innerText = 'Connected...';
+        connectBtn.setAttribute('id', 'disconnectPiBtn');
+        connectBtn.onclick = () => this.Disconnect()
+
+        this.webSocketClient.addEventListener('message', (event) => this.OnMessage(event))
     }
 
     Disconnect(): void {
